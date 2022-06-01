dd7362429b29e08d990699276b29df18a88b21e6
David Kang <davidwskang@gmail.com>
2022-06-01 17:06:42 -0400

    update

---
+++ libraryc/src/main/java/com/davidwskang/librarythree/MyClass.kt
@@ -0,0 +1,12 @@
+package com.davidwskang.librarythree
+
+class MyClass {
+	val test = listOf(
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+	)
+}
---
+++ libraryd/src/main/java/com/davidwskang/libraryd/MyClass.kt
@@ -0,0 +1,12 @@
+package com.davidwskang.libraryd
+
+class MyClass {
+	val test = listOf(
+		"",
+		"",
+		"",
+		"",
+		"",
+		"",
+	)
+}
